c1211efe377041f60d50bb50eb6e7a1d03fbef26
Rasmus <rasmus-strange@hotmail.com>
2018-10-05 10:26:41 +0200

    Test commit

---
+++ src/DiceTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiceTest {
+
+    @Test
+    void roll() {
+        
+    }
+
+    @Test
+    void rollMultiple() {
+
+    }
+}
